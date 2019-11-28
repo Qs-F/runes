90c4111f5502e1db162d63f5c28748a0219956a6
Qs-F <qsf@de-liker.com>
2019-11-28 22:41:12 +0900

    Add doc

--- index.go
+++ index.go
@@ -5,6 +5,7 @@ import (
 	"unicode/utf8"
 )
 
+// Index returns index number in rune byte
 func Index(s, substr string) int {
 	n := strings.Index(s, substr)
 	if n < 0 {
@@ -16,8 +17,8 @@ func Index(s, substr string) int {
 func Next(s string, bytepos int) int {
 	// 6 is the max value of utf8 byte
 	for count := 0; count < 6; count++ {
-		if utf8.RuneStart(s[bytepos+1+count]) {
-			return bytepos + count
+		if utf8.RuneStart(s[bytepos+count+1]) {
+			return bytepos + count + 1
 		}
 	}
 	return -1
