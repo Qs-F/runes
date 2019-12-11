0cc1727b38ee5cfe7905b6681d0ec999f3ffc3dc
Qs-F <qsf@de-liker.com>
2019-12-11 16:50:47 +0900

    Add tests

--- index_test.go
+++ index_test.go
@@ -2,6 +2,69 @@ package runes
 
 import "testing"
 
+func TestCount(t *testing.T) {
+	type Input struct {
+		S string
+		N int
+	}
+	tests := []struct {
+		Input Input
+		Must  int
+	}{
+		{
+			Input: Input{
+				S: "あいう",
+				N: -1,
+			},
+			Must: 3,
+		},
+		{
+			Input: Input{
+				S: "あ",
+				N: 2,
+			},
+			Must: 1,
+		},
+		{
+			Input: Input{
+				S: "あ",
+				N: 0,
+			},
+			Must: 0,
+		},
+		{
+			Input: Input{
+				S: "あ",
+				N: 1,
+			},
+			Must: 0,
+		},
+		{
+			Input: Input{
+				S: "あ",
+				N: 2,
+			},
+			Must: 0,
+		},
+		{
+			Input: Input{
+				S: "あい",
+				N: 4,
+			},
+			Must: 1,
+		},
+	}
+
+	for _, test := range tests {
+		c := Count(test.Input.S, test.Input.N)
+		if c != test.Must {
+			t.Errorf("expect: %d but get %d for %s\n", test.Must, c, test.Input.S)
+		} else {
+			t.Logf("get %d for %s\n", c, test.Input.S)
+		}
+	}
+}
+
 func TestIndex(t *testing.T) {
 	tests := []struct {
 		Input []string
@@ -52,6 +115,13 @@ func TestNext(t *testing.T) {
 				P: 2,
 			},
 			Must: 3,
+		},
+		{
+			Input: i{
+				S: "あいうえお日本語",
+				P: 3,
+			},
+			Must: 6,
 		},
 	}
 
