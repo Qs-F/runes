211efadd764f998c95ad5e540d03655754ca2acb
Qs-F <qsf@de-liker.com>
2019-12-12 16:09:36 +0900

    Add Count, Before, IndexAll funcs (IDK why but git add -p does not work)

--- index.go
+++ index.go
@@ -12,12 +12,37 @@ func Index(s, substr string) int {
 	if n < 0 {
 		return n
 	}
-	return utf8.RuneCountInString(s[:n])
+	return Count(s, n)
+}
+
+// Count returns the number of str in rune.
+// If bytecnt >= 0, then counts str[:bytescnt].
+// If bytecnt < 0 (mostly -1), then counts all the str.
+func Count(str string, bytecnt int) int {
+	if bytecnt < 0 {
+		return utf8.RuneCountInString(str)
+	}
+	fmt.Println([]byte(str)[:bytecnt])
+	return utf8.RuneCountInString(str[:bytecnt])
+}
+
+func IndexAll(s, substr string) []int {
+	return nil
 }
 
 func Next(s string, bytepos int) int {
 	// 6 is the max value of utf8 byte
-	for count := 0; count < 6; count++ {
+	for count := 0; count < 7; count++ {
+		if utf8.RuneStart(s[bytepos+count+1]) {
+			return bytepos + count + 1
+		}
+	}
+	return -1
+}
+
+func Before(s string, bytepos int) int {
+	// 6 is the max value of utf8 byte
+	for count := 0; count < 7; count++ {
 		if utf8.RuneStart(s[bytepos+count+1]) {
 			return bytepos + count + 1
 		}
