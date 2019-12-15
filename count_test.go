d7592093c396391769d2e51d89581bc5f45bc80f
Qs-F <qsf@de-liker.com>
2019-12-16 00:34:00 +0900

    Fix test cases

--- count_test.go
+++ count_test.go
@@ -30,37 +30,30 @@ func TestCount(t *testing.T) {
 				S: "あ",
 				N: 0,
 			},
-			Must: 0,
+			Must: 1,
 		},
 		{
 			Input: Input{
 				S: "あ",
 				N: 1,
 			},
-			Must: 0,
-		},
-		{
-			Input: Input{
-				S: "あ",
-				N: 2,
-			},
-			Must: 0,
+			Must: 1,
 		},
 		{
 			Input: Input{
 				S: "あい",
 				N: 4,
 			},
-			Must: 1,
+			Must: 2,
 		},
 	}
 
 	for _, test := range tests {
 		c := Count(test.Input.S, test.Input.N)
 		if c != test.Must {
-			t.Errorf("expect: %d but get %d for %s\n", test.Must, c, test.Input.S)
+			t.Errorf("expect: %d but get %d for %s[%d]\n", test.Must, c, test.Input.S, test.Input.N)
 		} else {
-			t.Logf("get %d for %s\n", c, test.Input.S)
+			t.Logf("get %d for %s[%d]\n", c, test.Input.S, test.Input.N)
 		}
 	}
 }
