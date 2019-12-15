227a363618b9de0495c1c8b8d6486ad587ea708e
Qs-F <qsf@de-liker.com>
2019-12-16 00:05:52 +0900

    Add test for Before

--- seek_test.go
+++ seek_test.go
@@ -2,6 +2,54 @@ package runes
 
 import "testing"
 
+func TestBefore(t *testing.T) {
+	type i struct {
+		S string
+		P int
+	}
+	tests := []struct {
+		Input i
+		Must  int
+	}{
+		{
+			Input: i{
+				S: "abcdef",
+				P: 2,
+			},
+			Must: 1,
+		},
+		{
+			Input: i{
+				S: "abc",
+				P: 3,
+			},
+			Must: 2,
+		},
+		{
+			Input: i{
+				S: "あいう",
+				P: 5,
+			},
+			Must: 3,
+		},
+		{
+			Input: i{
+				S: "あい",
+				P: 6,
+			},
+			Must: 3,
+		},
+	}
+
+	for _, test := range tests {
+		if n := Before(test.Input.S, test.Input.P); n != test.Must {
+			t.Errorf("expect %d but get %d for %s[%d]\n", test.Must, n, test.Input.S, test.Input.P)
+		} else {
+			t.Logf("get %d for %s[%d]\n", n, test.Input.S, test.Input.P)
+		}
+	}
+}
+
 func TestNext(t *testing.T) {
 	type i struct {
 		S string
