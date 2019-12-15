fec19bb00ed089ec518dd7654ca4064e2a21aa4c
Qs-F <qsf@de-liker.com>
2019-12-16 00:52:21 +0900

    Ref to invert the position of Before and After

--- seek.go
+++ seek.go
@@ -4,27 +4,6 @@ import "unicode/utf8"
 
 const MAX_BYTE_UTF8 = 6 // 6 is the max value of utf8 byte
 
-// Next returns the byte start position of next valid rune.
-// If the next valid rune is not found, returns given bytepos.
-func Next(s string, bytepos int) int {
-	// check bytepos exceeds max possible length
-	if bytepos > len(s)-1 {
-		return bytepos
-	}
-
-	max := bytepos + 1 + MAX_BYTE_UTF8
-	if max > len(s)-1 {
-		max = len(s) - 1
-	}
-
-	for cursor := bytepos + 1; cursor < max; cursor++ { // count starts from 1, cuz this is Next()
-		if utf8.RuneStart(s[cursor]) {
-			return cursor
-		}
-	}
-	return bytepos
-}
-
 // Before returns the byte start position of before valid rune.
 // If the before valid rune is not found, returns given bytepos.
 func Before(s string, bytepos int) int {
@@ -39,6 +18,27 @@ func Before(s string, bytepos int) int {
 	}
 
 	for cursor := max; cursor > bytepos-1-MAX_BYTE_UTF8; cursor-- {
+		if utf8.RuneStart(s[cursor]) {
+			return cursor
+		}
+	}
+	return bytepos
+}
+
+// Next returns the byte start position of next valid rune.
+// If the next valid rune is not found, returns given bytepos.
+func Next(s string, bytepos int) int {
+	// check bytepos exceeds max possible length
+	if bytepos > len(s)-1 {
+		return bytepos
+	}
+
+	max := bytepos + 1 + MAX_BYTE_UTF8
+	if max > len(s)-1 {
+		max = len(s) - 1
+	}
+
+	for cursor := bytepos + 1; cursor < max; cursor++ { // count starts from 1, cuz this is Next()
 		if utf8.RuneStart(s[cursor]) {
 			return cursor
 		}
