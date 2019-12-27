2ccc1af0d56ec2dfae75bb71f9c420b9d4f7149d
Qs-F <qsf@de-liker.com>
2019-12-28 04:43:43 +0900

    Add test for IndexAll

--- index_test.go
+++ index_test.go
@@ -1,6 +1,9 @@
 package runes
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIndex(t *testing.T) {
 	tests := []struct {
@@ -32,6 +35,29 @@ func TestIndex(t *testing.T) {
 
 	for _, test := range tests {
 		if n := Index(test.Input[0], test.Input[1]); n != test.Must {
+			t.Errorf("expect %d but get %d for '%s' of '%s'\n", test.Must, n, test.Input[0], test.Input[1])
+		} else {
+			t.Logf("get %d for '%s' of '%s'\n", test.Must, test.Input[0], test.Input[1])
+		}
+	}
+}
+
+func TestIndexAll(t *testing.T) {
+	tests := []struct {
+		Input []string
+		Must  []int
+	}{
+		{
+			Input: []string{
+				"abcabcabc",
+				"abc",
+			},
+			Must: []int{0, 3, 6},
+		},
+	}
+
+	for _, test := range tests {
+		if n := IndexAll(test.Input[0], test.Input[1]); !reflect.DeepEqual(n, test.Must) {
 			t.Errorf("expect %d but get %d for '%s' of '%s'\n", test.Must, n, test.Input[0], test.Input[1])
 		} else {
 			t.Logf("get %d for '%s' of '%s'\n", test.Must, test.Input[0], test.Input[1])
